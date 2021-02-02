a4696a6323c1a5bc3f6e1c68763fa8709559f3d4
Pedro <pedropaulo3521@gmail.com>
2021-02-02 14:10:12 -0300

    Começado o categoria-form.ts

--- src/app/pages/categorias/categoria-form/categoria-form.component.ts
+++ src/app/pages/categorias/categoria-form/categoria-form.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Categoria } from 'src/app/models/categoria.model';
+import { CategoriaService } from 'src/app/services/categoria.service';
 
 @Component({
   selector: 'app-categoria-form',
   templateUrl: './categoria-form.component.html',
   styleUrls: ['./categoria-form.component.css']
 })
-export class CategoriaFormComponent implements OnInit {
+export class CategoriaFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  acao: string; //novo ou edicao
+  categoriaForm: FormGroup;
+  errors: string[] = [];
+  submittingForm = false;
+  categoria = new Categoria;
+  titulo: string;
 
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private categoriaService: CategoriaService,
+    private formBuilder: FormBuilder
+  ) { }
+  
   ngOnInit() {
+    this.setAcao();
+    this.buildForm();
+    if(this.acao == 'edicao'){
+      this.getCategoria();
+    }
+  }
+  
+  ngAfterContentChecked(): void {
+   console.log("")
+  }
+
+  //Seta se a acao e um cadastro de uma categoria uma edicao
+  private setAcao(){
+    this.route.snapshot.url[0].path == 'novo'? this.acao = 'novo' : this.acao = 'edicao';
+  }
+
+  //Constroi o formGroup para controle do form
+  private buildForm(){
+   this.categoriaForm = this.formBuilder.group({
+      id:[null],
+      nome:[null, Validators.required, Validators.minLength(2)],
+      descricao: [null]
+    })
+  }
+
+  //Pega uma categoria para ser editada do banco
+  private getCategoria(){
+    this.route.paramMap.pipe(
+      //O + converte para number
+      switchMap(params => this.categoriaService.getById(+params.get('id')))
+    )
+    .subscribe(
+      categoria =>{
+        this.categoria = categoria;
+        //Adicionado os valores da categoria no form
+        this.categoriaForm.patchValue(this.categoria)
+      },
+      error => alert('Deu erro')
+    )
   }
 
 }
